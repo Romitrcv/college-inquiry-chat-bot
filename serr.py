9e8ffe9b348ffee2cd01c3414cad662b27a24eb8
sachin4utandon <sachin.tandon_cs@outlook.com>
2018-08-03 22:28:36 +0530

    Update serr.py

--- serr.py
+++ serr.py
@@ -1,6 +1,6 @@
 from googleapiclient.discovery import build
-my_api_key = "AIzaSyA0eQqo5wF6_i8oVwSg-rbZHBe8uNNHDT8"
-my_cse_id = "016248527696431796891:g3u5ie8wme4"
+my_api_key = "vvhhggfgdfgdfgchvhjbjj-khjkhfhg"
+my_cse_id = "446567767868868776564535437"
 
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
